4da46fc67284dcbc6ed3702b8252e6c735b7be2b
Adarsh <adarshpattanayak2004@gmail.com>
2026-09-19 18:48:26 +0530

    commit - "some minor changes before deploying"

--- backend/server.js
+++ backend/server.js
@@ -90,6 +90,6 @@ app.get("/", (req, res) => {
   res.send("helllo world !!!");
 });
 
-server.listen(port, () => {
+server.listen(port, "0.0.0.0", () => {
   console.log(`Server is running on port ${port}`);
 });
